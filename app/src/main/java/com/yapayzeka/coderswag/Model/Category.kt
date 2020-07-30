643612fdd7a21b0cf421b50a914b4a3db8baeca7
Tekin BASOZ <tekin@yapayzeka.com>
2020-07-30 17:29:23 +0300

    lesson 02

--- app/src/main/java/com/yapayzeka/coderswag/Model/Category.kt
+++ app/src/main/java/com/yapayzeka/coderswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package com.yapayzeka.coderswag.Model
 
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
